ba85f654aece37f6c2145bb684c7399e04f509fc
shambhu kumar <shambhu_kumar@shambhu.local>
2020-02-23 21:20:26 +0530

    corrected terrafrom code

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,24 +1,6 @@
-/*
 resource "aws_instance" "myweb" {
-  ami = "ami-05695932c5299858a"
-  instance_type = var.instance_type
+   ami = "${var.ami}"
+   instance_type = "${var.instance_type}"
+   security_groups = ["default"]
 
-}
-*/
-
-/*resource "aws_instance" "myweb" {
-  ami = "ami-05695932c5299858a"
-  instance_type = "${lookup(var.instance_type, terraform.workspace)}"
-  security_groups = ["default"]
-
-}
-*/
-resource "aws_instance" "myweb" {
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  security_groups = ["default"]
-
-    tags {
-      Managed = "terraform"
-    }
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,14 +1,5 @@
-/*variable "instance_type" {
-  default = "t2.micro"
-}
-*/
-variable "instance_type" {
-  type = map
-  #ami = "ami-05695932c5299858a"
+#Add the provider "
 
-  default = {
-    default = "t2.nano"
-    dev = "t2.micro"
-    prd = "t2.medium"
-  }
+provider "aws" {
+  region = "ap-south-1"
 }
