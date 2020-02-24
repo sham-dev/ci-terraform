7cc2a74ef5c70487b38753960a62462ef715d64c
sham-dev <61356395+sham-dev@users.noreply.github.com>
2020-02-24 14:46:16 +0530

    Update providers.tf

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -2,7 +2,6 @@
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4IAE6FTPJQTGBKJF"
-  secret_key = "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"
+  shared_credentials_file = "/home/ec2-user/.aws/credentials"
 
 }
