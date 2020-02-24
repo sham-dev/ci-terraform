cf0049fac5c7d1cdcdbe144c6960c6da04d863e7
sham-dev <61356395+sham-dev@users.noreply.github.com>
2020-02-24 14:42:20 +0530

    Update providers.tf

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -2,7 +2,7 @@
 
 provider "aws" {
   region = "ap-south-1"
-  access_key_id = "AKIA4IAE6FTPJQTGBKJF"
-  secret_access_key = "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"
+  access_key = "AKIA4IAE6FTPJQTGBKJF"
+  secret_key = "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"
 
 }
